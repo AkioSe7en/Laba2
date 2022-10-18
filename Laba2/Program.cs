08c609eddcbb7d4440a1b90a00ca6ef424e4e5fb
AkioSe7en <akiose7en@bk.ru>
2022-10-19 00:09:55 +0700

    Создана БАЗА

---
+++ Laba2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Laba2;
+
+var stocks = new List<Stock>
+{
+    new() {Town = "Бийск"},
+    new() {Town = "Барнаул"},
+    new() {Town = "село Черга"},
+    new() {Town = "Санкт-Петербург"}
+};
+    
+var cars = CarGenerator.GetCars(stocks);
